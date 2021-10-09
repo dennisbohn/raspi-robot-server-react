e60c0958307b907412242d48dcc0c54774039663
dennisbohn <dennis.bohn@bohnmedia.de>
2021-10-09 16:38:51 +0200

    first commit

---
+++ src/Components/Button.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import "./Button.css";
+
+class Button extends Component {
+  render() {
+    return <button className="Button">Click me!</button>;
+  }
+}
+
+export default Button;
