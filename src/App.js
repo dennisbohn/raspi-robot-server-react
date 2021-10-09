6e427b306611cdb64473f01bbaf805eb05cc6543
dennisbohn <dennis.bohn@bohnmedia.de>
2021-10-09 18:18:25 +0200

    socket.io hinzugefügt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
 import Button from "./Components/Button";
 import "./App.css";
 
 function App() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io(`https://${window.location.hostname}:3000`);
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [setSocket]);
+
   return (
     <div className="App">
       <Button />
